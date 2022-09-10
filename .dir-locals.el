645526cffaaada4a5f650d0a7e6c771fccededd5
ParetoOptimalDev <pareto.optimal@mailfence.com>
2022-09-10 14:22:45 -0500

    update .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
 ;; -*- lexical-binding: t -*-
-((nil . ((eval . (setq-local org-roam-directory (expand-file-name "~/ParetoOptimalDev/org")))
-	 (eval . (setq-local org-roam-db-location (expand-file-name "~/ParetoOptimalDev/org/org-roam.db"))))))
+((nil . ((eval . (setq-local org-roam-directory (expand-file-name "~/ParetoOptimalDev.github.io/org")))
+	 (eval . (setq-local org-roam-db-location (expand-file-name "~/ParetoOptimalDev.github.io/org/org-roam.db"))))))
